c80ba1b09cb0173a5bcb34ea0996210e1d7596d8
HB <you@example.com>
2020-09-29 17:40:17 +0200

    fan traits set more explicitly, ignore pycache

--- custom_components/empty_custom_fan/fan/empty_custom_fan.cpp
+++ custom_components/empty_custom_fan/fan/empty_custom_fan.cpp
@@ -7,8 +7,13 @@ namespace empty_custom_fan {
 static const char *TAG = "empty_custom_fan.fan";
 
 void EmptyCustomFan::setup() {
-  auto traits = fan::FanTraits(this->oscillating_ != nullptr, false, this->direction_ != nullptr);
+  auto traits = fan::FanTraits();
+  traits.set_direction(false);
+  traits.set_oscillation(false);
+  traits.set_speed(false);
+  
   this->fan_->set_traits(traits);
+  
   this->fan_->add_on_state_callback([this]() { this->next_update_ = true; });
 }
 
