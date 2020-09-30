ff81f6a7622cebd840ade40b26d06612d33bdb3e
HB <you@example.com>
2020-09-30 03:31:20 +0200

    cosmetics cv i2c

--- custom_components/empty_i2c_component/__init__.py
+++ custom_components/empty_i2c_component/__init__.py
@@ -5,12 +5,14 @@ from esphome.const import CONF_ID
 
 DEPENDENCIES = ['i2c']
 
+CONF_I2C_ADDR = 0x01
+
 empty_i2c_component_ns = cg.esphome_ns.namespace('empty_i2c_component')
 EmptyI2CComponent = empty_i2c_component_ns.class_('EmptyI2CComponent', cg.Component, i2c.I2CDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(EmptyI2CComponent)
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x29))
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
--- custom_components/empty_i2c_sensor/sensor.py
+++ custom_components/empty_i2c_sensor/sensor.py
@@ -5,12 +5,14 @@ from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ['i2c']
 
+CONF_I2C_ADDR = 0x01
+
 empty_i2c_sensor_ns = cg.esphome_ns.namespace('empty_i2c_sensor')
 EmptyI2CSensor = empty_i2c_sensor_ns.class_('EmptyI2CSensor', cg.PollingComponent, i2c.I2CDevice)
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
     cv.GenerateID(): cv.declare_id(EmptyI2CSensor),
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x5A))
+}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
